9e295870fe5cb7fd0409ceb578f2e918455dd6a9
Alex <nemokervi@yahoo.fr>
2014-05-06 11:08:35 +0800

    Version 0.0.0
    Initial init, mise en place de la structure, débroussaillage des flux
    Rss.

---
+++ content/js/app.js
@@ -0,0 +1,60 @@
+$( document ).ready(function() {
+
+	var couldMaximize = true;
+
+	var gui = require('nw.gui');
+	var win = gui.Window.get();
+
+	$('#close').click(function(){
+		window.close();
+	});
+
+	$('#reduce').click(function(){
+		win.minimize();
+	});
+
+	$('#maximise').click(function(){
+		if(couldMaximize == true){
+			win.maximize();
+			couldMaximize = false;
+		}else if(couldMaximize == false){
+			win.unmaximize();
+			couldMaximize = true;
+		}
+	});
+
+	$('#parametre').click(function(){
+		win.showDevTools();
+	});
+
+	$('#reload').click(function(){
+		win.reload();
+	});
+
+	$.ajax( {
+            type: "GET",
+            url: "http://feeds.feedburner.com/crunchgear",
+            dataType: "xml",
+            success: function(xml){
+                       $(xml).find('item').each(function(){
+                            var title = $(this).find('title').text();
+                            var reducetitle = title.slice(0,40) + ' ...';
+                            var url = $(this).find('url').text();
+                            var thumbnail = 'http://placekitten.com/300/200';
+                            $('<div></div>').html('<a data-url="http://google.com" class="thumbnail"><span class="glyphicon glyphicon-remove removeRss"></span><img src="' + thumbnail + '" /> <p>' + reducetitle + '</p></a>').appendTo('#contentRss');
+                      });
+        	}
+    });
+
+$('#thumbnail').click(function(){
+	var lien = $(this).data('url');
+	gui.Shell.openExternal(lien);
+	alert(lien);
+});
+
+
+	$('#btnTwitter').tab('show');
+	$('#btnGoogle').tab('show');
+	 $('#btnRss').tab('show');
+
+});
