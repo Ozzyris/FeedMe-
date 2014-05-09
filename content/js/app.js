079191dd539ea82fa6869de73c7bc1596cf5ce29
Alex <nemokervi@yahoo.fr>
2014-05-09 23:45:48 +0800

    Twitter begining
    Implementation du flux twitter. Ajout du loader.

--- content/js/app.js
+++ content/js/app.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 $( document ).ready(function() {
 
 	var couldMaximize = true;
@@ -14,10 +16,10 @@ $( document ).ready(function() {
 	});
 
 	$('#maximise').click(function(){
-		if(couldMaximize == true){
+		if(couldMaximize === true){
 			win.maximize();
 			couldMaximize = false;
-		}else if(couldMaximize == false){
+		}else if(couldMaximize === false){
 			win.unmaximize();
 			couldMaximize = true;
 		}
@@ -40,7 +42,7 @@ $( document ).ready(function() {
                        $(xml).find('item').each(function(){
                        		$('#HelloRss').hide();
                             var title = $(this).find('title').text();
-                            var reducetitle = title.slice(0,40) + ' ...';
+                            var reducetitle = title.slice(0,30) + ' ...';
                             var url = $(this).find('url').text();
                             var thumbnail = 'http://placekitten.com/300/200';
                             $('<div></div>').html('<a data-url="http://google.com" class="thumbnail"><span class="glyphicon glyphicon-remove removeRss"></span><img src="' + thumbnail + '" /> <p>' + reducetitle + '</p></a>').appendTo('#contentRss');
@@ -48,15 +50,30 @@ $( document ).ready(function() {
         	}
     });
 
+
+$('#btnTwitter').click(function(){
+	$('#HelloTwitter').hide();
+	$('#PreloaderTwitter').show();
+
+	request('https://twitter.com/search?q=collective%20intelligence', function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			$('#PreloaderTwitter').hide();
+			alert(body);
+		}else{
+			alert(error);
+		}
+	});
+});
+
 $('#thumbnail').click(function(){
 	var lien = $(this).data('url');
 	gui.Shell.openExternal(lien);
 	alert(lien);
 });
 
-
-	$('#btnTwitter').tab('show');
+	$('#btnTodolist').tab('show');
 	$('#btnGoogle').tab('show');
+	$('#btnTwitter').tab('show');
 	$('#btnRss').tab('show');
 
 });
